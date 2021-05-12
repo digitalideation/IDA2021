bb4fe784c965bc00c00527fb5eda2791733ce3fe
Sira <79851421+siratrinkler@users.noreply.github.com>
2021-05-12 08:24:49 +0200

    Add files via upload

---
+++ sira_trinkler/process/daily progress/day12/javascript/js/changeAttr.js
@@ -0,0 +1,9 @@
+function changeContent() {
+    alert("Hallo");
+    var elements = document.getElementsByTagName("article")
+    console.log(elements);
+    elements(0).setAttribute("class, MyClass")
+
+    
+
+}
