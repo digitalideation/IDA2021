317d2f5d81247b213db7a0ee1d0fcecf951e2e89
hzuellig <zuellig@internauta.ch>
2021-05-20 13:26:11 +0200

    Add David

---
+++ david_nicolai_soruco_herold/proscess/7_mai/java.js
@@ -0,0 +1,30 @@
+/*alert("Hallo")*/
+
+let meineZahl = 5;
+
+let name="David"
+
+let status =false;
+
+
+/*meineZahl
+
+console. log(summe);
+
+meineZahl = "Nummer Fünf";
+
+console.log(meineZahl);
+
+document.write(meineZahl)*/
+
+/*if(status){
+    document.write("Ja der Status ist true");
+}*/
+
+/*if (meineZahl == 10){
+    document.write("Ja meine Zahl hat den Wert 10")
+}*/
+
+var element = document.getElementById("titel");
+console.log(element);
+element.setAttribute("class", "red");
