54e3683e6bb4e337d9c82e27cc3e8e18e6a748ae
Nick Niles <nick@nickniles.com>
2021-05-26 08:07:37 +0200

    [feat] theme toggler

--- 00_exercise-files/day22/themeing/js/app.js
+++ 00_exercise-files/day22/themeing/js/app.js
@@ -1,8 +1,8 @@
-function handleNight() {
-    // Get the checkbox
-    var checkBox = document.getElementById("nightmode");
-    console.log('handle night', checkBox.checked);
+function handleNight(event) {
+    // store the checkbox in a variable
+    var checkBox = event.target
 
+    // One way to handle the class:
     // If the checkbox is checked, display the output text
     if (checkBox.checked == true) {
         document.body.classList.add('night');
@@ -10,3 +10,14 @@ function handleNight() {
         document.body.classList.remove('night');
     }
 }
+
+// This is one way to listen for the click event
+document.getElementById('nightcheck').onclick = handleNight;
+
+// This is another way to listen for the event
+// and toggle the class:
+var nightButton = document.getElementById('nightbutton');
+nightButton.addEventListener('click', function() {
+    console.log('just toggling')
+    document.body.classList.toggle('night')
+});
