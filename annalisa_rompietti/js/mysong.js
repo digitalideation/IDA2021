2a9b8ca7d0f1acc9448d29cf80f7b9663de8e175
hzuellig <zuellig@internauta.ch>
2021-05-27 18:17:41 +0200

    before pull

--- annalisa_rompietti/js/mysong.js
+++ annalisa_rompietti/js/mysong.js
@@ -1,6 +1,12 @@
-var tooinsistent = document.getElementById("tooinsistent")
-var button = document.getElementById("buttonplay")
+var tooinsistent = document.getElementById("tooinsistent");
+var button = document.getElementById("buttonplay");
+
+
 
 button.onclick = function(){
 tooinsistent.play();
 }
+
+/*button.addEventListener("click", function(){
+    tooinsistent.play();
+});*/
