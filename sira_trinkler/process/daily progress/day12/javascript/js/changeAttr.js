d15fe7ca9e5eb47c5f4f80dfd40a36acf13aca13
hzuellig <zuellig@internauta.ch>
2021-05-27 12:52:49 +0200

    Delete sira_trinkler directory

--- sira_trinkler/process/daily progress/day12/javascript/js/changeAttr.js
+++
@@ -1,9 +0,0 @@
-function changeContent() {
-    alert("Hallo");
-    var elements = document.getElementsByTagName("article")
-    console.log(elements);
-    elements(0).setAttribute("class, MyClass")
-
-    
-
-}
