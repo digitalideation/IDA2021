32f905d3af5268563a430a9d52c4ab570d96b94a
haniaslani <83285724+haniaslani@users.noreply.github.com>
2021-05-27 09:08:34 +0200

    Add files via upload

---
+++ hani/js/day12.js
@@ -0,0 +1,20 @@
+var youLikeMe = true;
+var myNum = 10;
+
+if (myNum == 10) {
+
+    document.write("Here is me");
+}
+else {
+
+    document.write("myNum is not 10")
+}
+
+
+
+var links = document.getElementsByTagName("a");
+for(i = 0; i < links.length; i++) {
+
+     links
+
+}
