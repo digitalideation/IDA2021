01c0c97d0491532a38257855270577a9b257b290
lulukagi <83285820+lulukagi@users.noreply.github.com>
2021-05-28 09:29:50 +0200

    Add files via upload

---
+++ julia_kagerbauer/js/day19.js
@@ -0,0 +1,117 @@
+
+var imgs= document.getElementsByClassName("arm--left");
+var arm= document.getElementsByClassName("arm--right");
+var B= document.getElementsByClassName("head");
+var C= document.getElementsByClassName("leg--left");
+var D= document.getElementsByClassName("leg--right");
+var A= document.getElementsByClassName("body");
+var audio = document.getElementsByClassName("musicplayer")
+var start = document.getElementsByClassName("startDance")
+var stop = document.getElementsByClassName("stopDance")
+console.log(imgs)
+console.log ("audio",audio)
+
+
+
+function toggleAnimation(){
+    console.log();
+    console.log("play pressed")
+
+    console.log("animation clicked")
+    
+    if (imgs[0].style.animationPlayState === 'paused' || !imgs[0].style.animationPlayState) {
+      imgs[0].style.animationPlayState = 'running';
+      console.log("running")
+      document.body.className = 'running';
+      audio[0].play();
+      stop[0].style.display= "flex"
+      start[0].style.display= "none"
+    } else {
+      imgs[0].style.animationPlayState = 'paused';
+      document.body.className = '';
+      audio[0].pause();
+      stop[0].style.display= "none"
+      start[0].style.display= "flex"
+    }
+
+    if (B[0].style.animationPlayState === 'paused' || !B[0].style.animationPlayState) {
+      B[0].style.animationPlayState = 'running';
+      console.log("running")
+      document.body.className = 'running';
+    } else {
+      B[0].style.animationPlayState = 'paused';
+      document.body.className = '';
+
+    }
+
+    if (C[0].style.animationPlayState === 'paused' || !C[0].style.animationPlayState) {
+      C[0].style.animationPlayState = 'running';
+      console.log("running")
+      document.body.className = 'running';
+    } else {
+      C[0].style.animationPlayState = 'paused';
+      document.body.className = '';
+
+    }
+
+    if (arm[0].style.animationPlayState === 'paused' || !arm[0].style.animationPlayState) {
+      arm[0].style.animationPlayState = 'running';
+      console.log("running")
+      document.body.className = 'running';
+    } else {
+      arm[0].style.animationPlayState = 'paused';
+      document.body.className = '';
+
+    }
+
+
+    if (D[0].style.animationPlayState === 'paused' || !D[0].style.animationPlayState) {
+      D[0].style.animationPlayState = 'running';
+      console.log("running")
+      document.body.className = 'running';
+    } else {
+      D[0].style.animationPlayState = 'paused';
+      document.body.className = '';
+
+    }
+
+    if (A[0].style.animationPlayState === 'paused' || !A[0].style.animationPlayState) {
+      A[0].style.animationPlayState = 'running';
+      console.log("running")
+      document.body.className = 'running';
+    } else {
+      A[0].style.animationPlayState = 'paused';
+      document.body.className = '';
+
+    }
+
+    }
+
+  console.log(imgs)
+
+  
+
+/*
+for (var i = 0; i < imgs.length; i++) {
+  /* imgs[i].onclick = toggleAnimation; 
+  imgs[i].style.animationPlayState = 'running';
+  console.log("Set all to running")
+}
+
+function toggleAnimation() {
+
+    console.log("animation clicked")
+  var style;
+  for (var i = 0; i < imgs.length; i++) {
+    style = imgs[i].style;
+    if (style.animationPlayState === 'running') {
+      style.animationPlayState = 'paused';
+      console.log("paused")
+      document.body.className = 'paused';
+    } else {
+      style.animationPlayState = 'running';
+      document.body.className = '';
+    }
+  }
+}
+*/
---
+++ julia_kagerbauer/js/day22.js
@@ -0,0 +1,31 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+ function text() {
+    var element = document.getElementsByClassName ("h1");
+    element.classList.toggle("h1-night");
+ }
+/*
+ function myFunction1() {
+    var element = document.getElementsByClassName("waves").src = "";;
+    element.classList.toggle("night");
+ } 
+
+ function before(){
+    document.getElementsByClassName('waves')
+    .src="img/photo1.jpg";
+    document.getElementById('message')
+    .innerHTML="Hii! GeeksforGeeks people";
+}
+  
+function after(){
+    document.getElementById('myImage')
+    .src="img/photo2.jpg";
+    document.getElementById('message')
+    .innerHTML="Bye! GeeksforGeeks people";
+}
+
+*/
+
+    
