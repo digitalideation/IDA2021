21574155b648a6b5e7cef7c1cc7fc1a4aec7c2ae
GinaBurri <69430435+GinaBurri@users.noreply.github.com>
2021-05-28 09:32:09 +0200

    Add files via upload

---
+++ gina_burri/process/Day22/daynight/js/app.js
@@ -0,0 +1,33 @@
+function handleNight(thing) {
+
+    // store the checkbox in a variable
+    var checkBox = thing.target
+    var myImg = document.getElementsByClassName('content')[0].getElementsByTagName('img')[0]
+    console.log('Image?', myImg)
+        // One way to handle the class:
+        // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+        document.body.classList.add('night');
+        myImg.setAttribute('src', 'img/riding.jpeg')
+        myImg.setAttribute('alt', 'Illustration of Red Riding Hood')
+        myImg.setAttribute('title', 'Illustration of Red Riding Hood')
+    } else {
+        document.body.classList.remove('night');
+        myImg.setAttribute('src', 'img/riding2.jpeg')
+        myImg.setAttribute('title', 'Different Illustration of Red Riding Hood')
+    }
+}
+
+// This is one way to listen for the click event
+document.getElementById('nightcheck').onclick = handleNight;
+
+
+// This is another way to listen for the event
+// and toggle the class:
+var nightButton = document.getElementById('nightbutton');
+
+nightButton.addEventListener('click', function() {
+    console.log('just toggling')
+    console.log(this)
+    document.body.classList.toggle('night')
+});
