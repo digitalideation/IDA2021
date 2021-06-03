d846ab90774a0f8fbaf34663901da1881892ee50
lissyrom <83285816+lissyrom@users.noreply.github.com>
2021-06-03 16:26:04 +0200

    Add files via upload

---
+++ annalisa_rompietti/js/track.js
@@ -0,0 +1,21 @@
+// const MouseEvents = document.getElementById('MouseEvents');
+// var lyric = [document.getElementById("lyric1, lyric2, lyric3, lyric4")]
+
+
+
+// for ( var i=1; i<=lyric.length; i++) {
+// console.log(lyric[i]);
+// }
+
+
+
+
+function mouseOver() {
+    document.getElementById("lyric1").style.color = "white";
+  }
+    
+function mouseOut() {
+    document.getElementById("lyric2").style.color = "white";
+}
+
+
